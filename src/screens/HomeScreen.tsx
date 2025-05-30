d9155950ca0341ea990dd2dd1c6b105f9e4a29f4
JaumC <jvvianachaves@gmail.com>
2025-05-30 13:27:43 -0300

    scroll vertical entre telas no mesmo lugar

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,22 +2,33 @@ import FooterBar from "../components/footerbar/FooterBar"
 import NavBar from "../components/navbar/NavBar"
 
 export default function HomeScreen() {
-
   return (
-    <div className="w-full h-full flex flex-col justify-between">
-      <div className="overflow-hidden fixed w-full">
+    <div className="w-full h-screen flex flex-col">
+      <div className="fixed w-full z-50">
         <NavBar />
       </div>
-      <div className="flex-1 overflow-auto pt-[80px] justify-between flex flex-col">
-        <div className="p-[20px]">
-          <p>a</p>
-          <p>a</p>
-          <p>a</p>
-        </div>
 
-        <div className=" w-full">
+      <div className="flex-1 overflow-y-scroll snap-y snap-mandatory pt-[80px] hide-scrollbar">
+        <section className="snap-start h-screen flex items-center justify-center bg-zinc-900">
+          <p className="text-2xl">Sessão 1</p>
+
+        </section>
+        <section className="snap-start h-screen flex items-center justify-center bg-zinc-900">
+          <p className="text-2xl">Sessão 2</p>
+        </section>
+        <section className="snap-start h-screen flex items-center justify-center bg-zinc-900">
+          <p className="text-2xl">Sessão 3</p>
+        </section>
+        <section className="snap-start h-screen flex items-center justify-center bg-zinc-900">
+          <p className="text-2xl">Sessão 4</p>
+        </section>
+        <section className="snap-start h-screen flex items-center justify-center bg-zinc-900">
+          <p className="text-2xl">Sessão 5</p>
+        </section>
+
+        <section className="snap-start h-screen flex items-end justify-center bg-zinc-900">
           <FooterBar />
-        </div>
+        </section>
       </div>
     </div>
   )
