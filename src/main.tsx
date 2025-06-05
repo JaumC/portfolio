31b1c3a2869ad57556b5018383a292c8b09ff607
JaumC <jvvianachaves@gmail.com>
2025-06-05 19:39:37 -0300

    Adicinando altura de tela dinâmica

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="w-full bg-[url('/fundo.jpg')] h-screen flex justify-center text-white bg-cover bg-center bg-no-repeat">
+    <div className="w-full bg-[url('/fundo.jpg')] h-[100dvh] flex justify-center text-white bg-cover bg-center bg-no-repeat">
       <HomeScreen/>
     </div>
   </StrictMode>,
