30b8c9efa03b59d82d8b8b2f6a5727e393955cc4
JaumC <jvvianachaves@gmail.com>
2025-06-05 19:47:52 -0300

    Corrigindo scroll

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -92,20 +92,20 @@ export default function HomeScreen() {
   }, [])
 
   return (
-    <div className="w-full h-screen flex flex-col">
+    <div className="w-full h-[100dvh] flex flex-col">
       <div className={`fixed top-0 left-0 w-full z-50 transform transition-transform duration-500 ease-in-out ${showNavBar ? "translate-y-0" : "-translate-y-full"}`}>
         <NavBar onScroll={handleScroll} activeSection={activeSection}/>
       </div>
 
-      <div ref={scrollRef} className="flex-1 overflow-y-scroll snap-y snap-mandatory pt-[80px] hide-scrollbar">
+      <div ref={scrollRef} className="flex-1 overflow-y-scroll snap-y snap-mandatory hide-scrollbar">
         {pages.map((page, index) => {
           return (
-            <section key={index} id={page} className="snap-start h-screen flex items-center justify-center">
+            <section key={index} id={page} className="snap-start h-[100dvh] scroll-mt-20 flex items-center justify-center">
               {pagesComponents[page]}
             </section>
           )
         })}
-        <section className="snap-start h-screen flex items-end justify-center">
+        <section className="snap-start h-[100dvh] scroll-mt-20 flex items-end justify-center">
           <FooterBar />
         </section>
       </div>
