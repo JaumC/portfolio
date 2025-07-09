b4f8c6044c34c3a8e0ef0791e28b7b199bd25321
JaumC <jvvianachaves@gmail.com>
2025-07-09 10:23:18 -0300

    setVH

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,21 @@ import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import './index.css'
 
+// Corrige altura da viewport mobile
+const setVH = () => {
+  const vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+}
+setVH()
+window.addEventListener('resize', setVH)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="w-full bg-[url('/fundo.jpg')] h-[100dvh] flex justify-center text-white bg-cover bg-center bg-no-repeat">
-      <HomeScreen/>
+    <div
+      className="w-full bg-[url('/fundo.jpg')] flex justify-center text-white bg-cover bg-center bg-no-repeat"
+      style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
+    >
+      <HomeScreen />
     </div>
   </StrictMode>
 )
