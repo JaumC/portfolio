4a3584b7f524172801f45962468917e872a3c89f
JaumC <jvvianachaves@gmail.com>
2025-09-10 19:59:54 -0300

    fix: em construção screen

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,8 +1,14 @@
+import { FcEngineering } from "react-icons/fc";
 
 export default function AboutScreen() {
   return (
     <div className="w-full h-screen bg-[#FFF]">
-      
+      <div className="text-black h-full flex items-center justify-center text-xl font-semibold flex-col gap-2">
+        <p>
+          Em construção...
+        </p>
+        <FcEngineering className="animate-[spin_3s_linear_infinite]" size={80} />
+      </div>
     </div>
   )
 }
--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -1,6 +1,13 @@
+import { FcEngineering } from "react-icons/fc";
 
 export default function ContactScreen() {
   return (
-    <div className="text-black">ContactScreen</div>
-  )
+    <div className="w-full h-screen bg-[#FFF]">
+      <div className="text-black h-full flex items-center justify-center text-xl font-semibold flex-col gap-2">
+        <p>
+          Em construção...
+        </p>
+        <FcEngineering className="animate-[spin_3s_linear_infinite]" size={80} />
+      </div>
+    </div>)
 }
