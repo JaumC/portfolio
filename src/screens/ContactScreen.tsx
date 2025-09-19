85fb8df7491b7d3d47fa88a6dd51a0a6b65f7aa0
JaumC <jvvianachaves@gmail.com>
2025-09-19 16:21:19 -0300

    feat contados

--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -3,7 +3,20 @@ import FooterBar from "../components/footerbar/FooterBar";
 
 export default function ContactScreen() {
   return (
-    <div className="w-full h-screen bg-[#FFF]">
+    <div className="w-full h-screen bg-[#FFF] relative">
+
+      <div className="hidden lg:block absolute h-full w-full overflow-hidden">
+        <div className="h-full w-[120px] bg-[#2193CC] z-0 flex absolute right-50" />
+        <div className="h-full animate-move-up-delay w-[30px] bg-[#2193CC] z-0 flex absolute left-15" />
+        <div className="h-full animate-move-up w-[5px] bg-[#2193CC] z-0 flex absolute left-5" />
+        <div className="h-full animate-move-up-delay w-[10px] bg-[#2193CC] z-0 flex absolute right-35" />
+      </div>
+
+      <div className="lg:hidden text-black h-full flex items-center justify-center m-auto text-xl font-semibold flex-col gap-2">
+        <div className="h-full animate-move-up-delay w-[30px] bg-[#2193CC] z-0 flex absolute left-15" />
+        <div className="h-full animate-move-up w-[5px] bg-[#2193CC] z-0 flex absolute left-5" />
+        <div className="h-full animate-move-up-delay w-[10px] bg-[#2193CC] z-0 flex absolute right-35" />
+      </div>
       <div className="text-black h-full flex items-center justify-between text-xl font-semibold flex-col gap-2">
         <div className="flex flex-col items-center justify-center m-auto">
           <p>
